63129b2f4a570b4d51b5b9973741a5e961d1f3a0
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-05-12 20:01:22 -0300

    Logica ADC e calculo das misturas
    Refactored ADC reading and added calculations for height and volume based on ADC input. Introduced a mixing calculation for two components with defined percentages.

--- main.c
+++ main.c
@@ -1,45 +1,89 @@
 #include <stdio.h>
-#include <stdint.h>
 
 #include "pico/stdlib.h"
 
 #include "config.h"
+
 #include "adc.h"
 
 int main()
 {
     stdio_init_all();
 
+    sleep_ms(2000);
+
     adc_setup();
 
-    float nivel_anterior = -100.0f;
+    printf("Sistema iniciado!\n\n");
 
     while(true)
     {
-        uint16_t adc_raw = adc_read_filtered();
+  
+        // LEITURA ADC
+        
+        uint16_t adc_raw =
+            adc_read_raw();
 
-        float tensao = adc_to_voltage(adc_raw);
 
-        float nivel = adc_to_percent(adc_raw);
+        // CONVERSÕES
+        
 
-        if((nivel > (nivel_anterior + HISTERESE_PERCENT)) ||
-           (nivel < (nivel_anterior - HISTERESE_PERCENT)))
-        {
-            nivel_anterior = nivel;
-        }
+        float nivel_percentual =
+            adc_to_percent(adc_raw);
 
-        printf("\n");
+        float altura_cm =
+            percent_to_height(
+                nivel_percentual);
 
-        printf("ADC RAW: %d\n", adc_raw);
+        float volume_ml =
+            height_to_volume(
+                altura_cm);
 
-        printf("Tensao: %.2f V\n", tensao);
+        // ==========================================
+        // MISTURA
+        // ==========================================
 
-        printf("Nivel: %.1f %%\n", nivel_anterior);
+        float percentual_a =
+            PERCENTUAL_A;
 
-        printf("--------------------\n");
+        float percentual_b =
+            100.0f - percentual_a;
 
-        sleep_ms(TEMPO_LEITURA_MS);
+        float volume_a =
+            VOLUME_TOTAL_ML *
+            (percentual_a / 100.0f);
+
+        float volume_b =
+            VOLUME_TOTAL_ML *
+            (percentual_b / 100.0f);
+
+        // ==========================================
+        // SERIAL MONITOR
+        // ==========================================
+
+        printf("ADC: %d\n",
+               adc_raw);
+
+        printf("Nivel: %.2f %%\n",
+               nivel_percentual);
+
+        printf("Altura: %.2f cm\n",
+               altura_cm);
+
+        printf("Volume tanque: %.2f mL\n",
+               volume_ml);
+
+        printf("Mistura total: %.2f mL\n",
+               VOLUME_TOTAL_ML);
+
+        printf("A: %.2f %% -> %.2f mL\n",
+               percentual_a,
+               volume_a);
+
+        printf("B: %.2f %% -> %.2f mL\n\n",
+               percentual_b,
+               volume_b);
+
+        sleep_ms(200);
     }
-
-    return 0;
 }
