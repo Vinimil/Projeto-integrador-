e7407b641236ac7dbcfafcec365be0cb25141a33
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-05-12 19:58:59 -0300

    Conversão

--- adc.h
+++ adc.h
@@ -3,12 +3,14 @@
 
 #include <stdint.h>
 
-void adc_setup(void);
+void adc_setup();
 
-uint16_t adc_read_filtered(void);
-
-float adc_to_voltage(uint16_t adc_value);
+uint16_t adc_read_raw();
 
 float adc_to_percent(uint16_t adc_value);
+
+float percent_to_height(float percent);
+
+float height_to_volume(float height_cm);
 
 #endif
