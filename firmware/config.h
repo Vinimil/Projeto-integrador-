b8caa3e10f32247f992f0c86af79665a2df4cb54
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-05-16 22:02:14 -0300

    Agora o ultimo tanque e apenas uma saida sem medidas

--- firmware/config.h
+++ firmware/config.h
@@ -1,47 +1,65 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
 // ADC
 
+#define ADC_PIN_A          26
+#define ADC_CHANNEL_A      0
 
-#define ADC_PIN 26
-#define ADC_CHANNEL 0
-
-#define ADC_MIN 500
-#define ADC_MAX 3500
+#define ADC_PIN_B          27
+#define ADC_CHANNEL_B      1
 
 
+// CALIBRAÇÃO DOS TANQUES
+
+
+#define ADC_MIN_A          500
+#define ADC_MAX_A          60000
+
+#define ADC_MIN_B          500
+#define ADC_MAX_B          60000
+
+//-------------------------------------
 // FILTRO
+//-------------------------------------
+
+#define NUM_AMOSTRAS       5
+#define HISTERESE          3.0f
 
 
-#define NUM_AMOSTRAS 5
-
-#define HISTERESE 3.0f
+// TEMPO
 
 
-// TANQUE
+#define LOOP_DELAY_MS      200
+
+//-------------------------------------
+// REFERÊNCIA ADC
+//-------------------------------------
+
+#define ADC_VREF           3.3f
+#define ADC_RESOLUTION     65535.0f
 
 
-#define TANQUE_COMPRIMENTO_CM 34.5f
-
-#define TANQUE_LARGURA_CM 9.5f
-
-#define TANQUE_ALTURA_CM 21.5f
+// TANQUE RETANGULAR
 
 
-// MISTURA
+#define TANQUE_LARGURA_CM  34.5f
+#define TANQUE_COMPR_CM    9.5f
+#define TANQUE_ALTURA_CM   21.5f
 
 
-#define VOLUME_TOTAL_ML 2000.0f
-
-// usuário
-#define PERCENTUAL_A 40.0f
-
-//
 // BOMBAS
-// 
-#define BOMBA_A_PIN 15
-#define BOMBA_B_PIN 16
+
+
+#define BOMBA_A_PIN        14
+#define BOMBA_B_PIN        15
+
+
+// MISTURA (TESTE)
+
+
+#define VOLUME_TOTAL_ML    1000.0f
+
+#define PERCENTUAL_A       30.0f
 
 #endif
