6a5321f5a829365f32cd93d355dd8c1740f7ac65
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-02 12:27:47 -0300

    Refactor ADC functions and add header file

--- firmware/adc.c
+++ firmware/adc.c
@@ -1,60 +1,16 @@
-#include <stdio.h>
+#ifndef ADC_H
+#define ADC_H
 
-#include "pico/stdlib.h"
-#include "hardware/adc.h"
+#include <stdint.h>
 
-#include "config.h"
-#include "adc.h"
+void adc_setup();
 
-void adc_setup()
-{
-    adc_init();
+uint16_t adc_read_raw();
 
-    adc_gpio_init(ADC_PIN);
+float adc_to_percent(uint16_t adc_value);
 
-    adc_select_input(ADC_CHANNEL);
-}
+float percent_to_height(float percent);
 
-uint16_t adc_read_raw()
-{
-    uint32_t soma = 0;
+float height_to_volume(float height_cm);
 
-    for(int i = 0; i < NUM_AMOSTRAS; i++)
-    {
-        soma += adc_read();
-
-        sleep_ms(5);
-    }
-
-    return soma / NUM_AMOSTRAS;
-}
-
-float adc_to_percent(uint16_t adc_value)
-{
-    float percent =
-        ((float)(adc_value - ADC_MIN) /
-        (ADC_MAX - ADC_MIN)) * 100.0f;
-
-    if(percent < 0.0f)
-        percent = 0.0f;
-
-    if(percent > 100.0f)
-        percent = 100.0f;
-
-    return percent;
-}
-
-float percent_to_height(float percent)
-{
-    return
-        (percent / 100.0f) *
-        TANQUE_ALTURA_CM;
-}
-
-float height_to_volume(float height_cm)
-{
-    return
-        TANQUE_COMPR_CM *
-        TANQUE_LARGURA_CM *
-        height_cm;
-}
+#endif
