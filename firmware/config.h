e3e420161413c2d54cc4205e89596e54f6aba7f6
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-02 19:02:43 -0300

    agora a bomba funciona

--- firmware/config.h
+++ firmware/config.h
@@ -1,15 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
 // ADC
-
 
 #define ADC_PIN              26
 #define ADC_CHANNEL          0
 
-#define ADC_MIN              500
-#define ADC_MAX              60000
+// Ajustar
+#define ADC_MIN              0
+#define ADC_MAX              4095
 
 #define NUM_AMOSTRAS         5
 
@@ -24,16 +23,14 @@
 
 // LOOP
 
+
 #define LOOP_DELAY_MS        100
 
-
 // DOSAGEM
-
 
 #define DOSE_MIN_ML          100
 #define DOSE_MAX_ML          3000
 #define DOSE_STEP_ML         50
-
 
 // BOMBA
 
@@ -42,10 +39,9 @@
 // overshoot
 #define COMPENSACAO_ML       25.0f
 
-// Vazão inicial estimada
+// Valor inicial para testes
 #define VAZAO_BOMBA_ML_S     100.0f
 
-// Tempo mínimo da bomba ligada
 #define TEMPO_MIN_BOMBA_MS   500
 
 #endif
