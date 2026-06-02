c9d4080e7ea412ab8f5682a06e54f946b5ca51ee
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-02 12:28:55 -0300

    Implement ADC setup and reading functions

--- firmware/adc.c
+++ firmware/adc.c
@@ -1,16 +1,52 @@
-#ifndef ADC_H
-#define ADC_H
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
 
-#include <stdint.h>
+#include "config.h"
+#include "adc.h"
 
-void adc_setup();
+// INIT
+void adc_setup()
+{
+    adc_init();
+    adc_gpio_init(ADC_PIN);
+    adc_select_input(ADC_CHANNEL);
+}
 
-uint16_t adc_read_raw();
+// FILTRO 
+uint16_t adc_read_raw()
+{
+    uint32_t soma = 0;
 
-float adc_to_percent(uint16_t adc_value);
+    for(int i = 0; i < NUM_AMOSTRAS; i++)
+    {
+        soma += adc_read();
+        sleep_ms(5);
+    }
 
-float percent_to_height(float percent);
+    return soma / NUM_AMOSTRAS;
+}
 
-float height_to_volume(float height_cm);
+// ADC -> %
+float adc_to_percent(uint16_t adc_value)
+{
+    float p =
+        ((float)(adc_value - ADC_MIN) /
+        (ADC_MAX - ADC_MIN)) * 100.0f;
 
-#endif
+    if(p < 0) p = 0;
+    if(p > 100) p = 100;
+
+    return p;
+}
+
+// % -> altura
+float percent_to_height(float percent)
+{
+    return (percent / 100.0f) * TANQUE_ALTURA_CM;
+}
+
+// altura -> volume
+float height_to_volume(float h)
+{
+    return TANQUE_COMPR_CM * TANQUE_LARGURA_CM * h * 10.0f;
+}
