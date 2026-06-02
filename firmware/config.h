1bb263cffdcac9ce712257479d7abba52c83c98a
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-02 18:56:38 -0300

    adição da bomba

--- firmware/config.h
+++ firmware/config.h
@@ -1,27 +1,51 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+
 // ADC
-#define ADC_PIN          26
-#define ADC_CHANNEL      0
 
-#define ADC_MIN          500
-#define ADC_MAX          60000
 
-// FILTRO
-#define NUM_AMOSTRAS     5
+#define ADC_PIN              26
+#define ADC_CHANNEL          0
+
+#define ADC_MIN              500
+#define ADC_MAX              60000
+
+#define NUM_AMOSTRAS         5
+
+
+// TANQUE
+
+
+#define TANQUE_COMPR_CM      33.7f
+#define TANQUE_LARGURA_CM    17.0f
+#define TANQUE_ALTURA_CM     12.8f
+
 
 // LOOP
-#define LOOP_DELAY_MS    100
 
-// TANQUE (cm)
-#define TANQUE_COMPR_CM    33.7f
-#define TANQUE_LARGURA_CM  17.0f
-#define TANQUE_ALTURA_CM   12.8f
+#define LOOP_DELAY_MS        100
+
 
 // DOSAGEM
-#define DOSE_MIN_ML   100
-#define DOSE_MAX_ML   3000
-#define DOSE_STEP     50
+
+
+#define DOSE_MIN_ML          100
+#define DOSE_MAX_ML          3000
+#define DOSE_STEP_ML         50
+
+
+// BOMBA
+
+#define BOMBA_PIN            15
+
+// overshoot
+#define COMPENSACAO_ML       25.0f
+
+// Vazão inicial estimada
+#define VAZAO_BOMBA_ML_S     100.0f
+
+// Tempo mínimo da bomba ligada
+#define TEMPO_MIN_BOMBA_MS   500
 
 #endif
