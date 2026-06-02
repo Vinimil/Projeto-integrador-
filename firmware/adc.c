b941ebd0c430cbc2626bb21eb9fce13a5c2d3069
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-02 20:02:05 -0300

    Refactor ADC functions for consistency and clarity

--- firmware/adc.c
+++ firmware/adc.c
@@ -4,16 +4,16 @@
 #include "config.h"
 #include "adc.h"
 
-// INIT
-void adc_setup()
+void adc_setup(void)
 {
     adc_init();
+
     adc_gpio_init(ADC_PIN);
+
     adc_select_input(ADC_CHANNEL);
 }
 
-// FILTRO 
-uint16_t adc_read_raw()
+uint16_t adc_read_raw(void)
 {
     uint32_t soma = 0;
 
@@ -26,27 +26,33 @@ uint16_t adc_read_raw()
     return soma / NUM_AMOSTRAS;
 }
 
-// ADC -> %
 float adc_to_percent(uint16_t adc_value)
 {
-    float p =
+    float percent =
         ((float)(adc_value - ADC_MIN) /
         (ADC_MAX - ADC_MIN)) * 100.0f;
 
-    if(p < 0) p = 0;
-    if(p > 100) p = 100;
+    if(percent < 0.0f)
+        percent = 0.0f;
 
-    return p;
+    if(percent > 100.0f)
+        percent = 100.0f;
+
+    return percent;
 }
 
-// % -> altura
 float percent_to_height(float percent)
 {
-    return (percent / 100.0f) * TANQUE_ALTURA_CM;
+    return
+        (percent / 100.0f) *
+        TANQUE_ALTURA_CM;
 }
 
-// altura -> volume
-float height_to_volume(float h)
+float height_to_volume(float height_cm)
 {
-    return TANQUE_COMPR_CM * TANQUE_LARGURA_CM * h * 10.0f;
+    return
+        TANQUE_COMPR_CM *
+        TANQUE_LARGURA_CM *
+        height_cm *
+        10.0f;
 }
