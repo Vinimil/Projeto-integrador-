4f2d2db33e622e4703c38012d0686c78c8ee9c76
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-02 19:55:01 -0300

    Update ADC parameters and add approximation settings

--- firmware/config.h
+++ firmware/config.h
@@ -1,56 +1,45 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
 // ADC
 
+#define ADC_PIN                26
+#define ADC_CHANNEL            0
 
-#define ADC_PIN              26
-#define ADC_CHANNEL          0
+#define ADC_MIN                500
+#define ADC_MAX                3500
 
-#define ADC_MIN              0
-#define ADC_MAX              4095
-
-#define NUM_AMOSTRAS         5
-
+#define NUM_AMOSTRAS           10
 
 // TANQUE
 
-
-#define TANQUE_COMPR_CM      33.7f
-#define TANQUE_LARGURA_CM    17.0f
-#define TANQUE_ALTURA_CM     12.8f
-
+#define TANQUE_COMPR_CM        33.7f
+#define TANQUE_LARGURA_CM      17.0f
+#define TANQUE_ALTURA_CM       12.8f
 
 // LOOP
 
-
-#define LOOP_DELAY_MS        100
-
+#define LOOP_DELAY_MS          100
 
 // DOSAGEM
 
-
-#define DOSE_MIN_ML          100
-#define DOSE_MAX_ML          3000
-#define DOSE_STEP_ML         50
-
+#define DOSE_MIN_ML            100
+#define DOSE_MAX_ML            3000
+#define DOSE_STEP_ML           50
 
 // BOMBA
 
+#define BOMBA_PIN              15
 
-#define BOMBA_PIN            15
+// compensação
 
-// CALIBRAÇÃO
+#define COMPENSACAO_ML         25.0f
 
+// aproximação
 
-// Quanto continua saindo após desligar
-#define COMPENSACAO_ML       25.0f
+#define JANELA_APROXIMACAO_ML  100.0f
 
-// Vazão estimada inicial
-#define VAZAO_BOMBA_ML_S     100.0f
-
-// Tempo mínimo de acionamento
-#define TEMPO_MIN_BOMBA_MS   500
+#define PULSO_LIGADO_MS        80
+#define PULSO_DESLIGADO_MS     120
 
 #endif
