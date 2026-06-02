a19e2108a605f13c89f55fbd91adf804d4f6835a
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-02 12:26:57 -0300

    Adição da dosagem
    Updated loop delay and added dosage parameters.

--- firmware/config.h
+++ firmware/config.h
@@ -1,38 +1,27 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
 // ADC
-
-
 #define ADC_PIN          26
 #define ADC_CHANNEL      0
-
-
-// CALIBRAÇÃO
-
 
 #define ADC_MIN          500
 #define ADC_MAX          60000
 
-
 // FILTRO
-
-
 #define NUM_AMOSTRAS     5
 
+// LOOP
+#define LOOP_DELAY_MS    100
 
-// TEMPO
-
-
-#define LOOP_DELAY_MS    200
-
-
-// TANQUE
-
-
+// TANQUE (cm)
 #define TANQUE_COMPR_CM    33.7f
 #define TANQUE_LARGURA_CM  17.0f
 #define TANQUE_ALTURA_CM   12.8f
+
+// DOSAGEM
+#define DOSE_MIN_ML   100
+#define DOSE_MAX_ML   3000
+#define DOSE_STEP     50
 
 #endif
