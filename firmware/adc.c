7f93457a20289ce61683827e4754ac5955ba0bf8
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-05 18:53:41 -0300

    Refactor height_to_volume to use calibration factor
    Updated height_to_volume function to include calibration factor.

--- firmware/adc.c
+++ firmware/adc.c
@@ -7,9 +7,7 @@
 void adc_setup(void)
 {
     adc_init();
-
     adc_gpio_init(ADC_PIN);
-
     adc_select_input(ADC_CHANNEL);
 }
 
@@ -43,16 +41,11 @@ float adc_to_percent(uint16_t adc_value)
 
 float percent_to_height(float percent)
 {
-    return
-        (percent / 100.0f) *
-        TANQUE_ALTURA_CM;
+    return (percent / 100.0f) * TANQUE_ALTURA_CM;
 }
 
 float height_to_volume(float height_cm)
 {
-    return
-        TANQUE_COMPR_CM *
-        TANQUE_LARGURA_CM *
-        height_cm *
-        10.0f;
+    // O volume teórico é multiplicado pelo fator de escala real
+    return (TANQUE_COMPR_CM * TANQUE_LARGURA_CM * height_cm) * FATOR_CALIBRACAO;
 }
