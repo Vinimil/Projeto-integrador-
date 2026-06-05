7fa64984ddc3f89a2c729bda750c92fa10955898
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-05 18:50:37 -0300

    primeiro teste de calibracao

--- firmware/config.h
+++ firmware/config.h
@@ -1,45 +1,36 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// ADC
 
 #define ADC_PIN                26
 #define ADC_CHANNEL            0
 
-#define ADC_MIN                500
-#define ADC_MAX                3500
+#define ADC_MIN                1315
+#define ADC_MAX                3957
 
-#define NUM_AMOSTRAS           10
 
-// TANQUE
+#define NUM_AMOSTRAS           200 
 
-#define TANQUE_COMPR_CM        33.7f
-#define TANQUE_LARGURA_CM      17.0f
-#define TANQUE_ALTURA_CM       12.8f
 
-// LOOP
+#define TANQUE_COMPR_CM        30.0f
+#define TANQUE_LARGURA_CM      15.0f
+#define TANQUE_ALTURA_CM       9.0f
 
+
+#define BOMBA_PIN              15
 #define LOOP_DELAY_MS          100
-
-// DOSAGEM
 
 #define DOSE_MIN_ML            100
 #define DOSE_MAX_ML            3000
 #define DOSE_STEP_ML           50
 
-// BOMBA
-
-#define BOMBA_PIN              15
-
-// compensação
 
 #define COMPENSACAO_ML         25.0f
 
-// aproximação
-
 #define JANELA_APROXIMACAO_ML  100.0f
+#define PULSO_LIGADO_MS        200 
+#define PULSO_DESLIGADO_MS     100
 
-#define PULSO_LIGADO_MS        80
-#define PULSO_DESLIGADO_MS     120
+#define FATOR_CALIBRACAO       1.0f 
 
 #endif
