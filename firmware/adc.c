c411a3467fe8326a293d67c0191aeb8ed0a2ede0
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-16 18:34:09 -0300

    Optimize adc_read_raw by removing sleep delay
    Removed sleep delay in adc_read_raw function to improve performance.

--- firmware/adc.c
+++ firmware/adc.c
@@ -7,7 +7,9 @@
 void adc_setup(void)
 {
     adc_init();
+
     adc_gpio_init(ADC_PIN);
+
     adc_select_input(ADC_CHANNEL);
 }
 
@@ -18,34 +20,7 @@ uint16_t adc_read_raw(void)
     for(int i = 0; i < NUM_AMOSTRAS; i++)
     {
         soma += adc_read();
-        sleep_ms(5);
     }
 
     return soma / NUM_AMOSTRAS;
-}
-
-float adc_to_percent(uint16_t adc_value)
-{
-    float percent =
-        ((float)(adc_value - ADC_MIN) /
-        (ADC_MAX - ADC_MIN)) * 100.0f;
-
-    if(percent < 0.0f)
-        percent = 0.0f;
-
-    if(percent > 100.0f)
-        percent = 100.0f;
-
-    return percent;
-}
-
-float percent_to_height(float percent)
-{
-    return (percent / 100.0f) * TANQUE_ALTURA_CM;
-}
-
-float height_to_volume(float height_cm)
-{
-    // O volume teórico é multiplicado pelo fator de escala real
-    return (TANQUE_COMPR_CM * TANQUE_LARGURA_CM * height_cm) * FATOR_CALIBRACAO;
 }
