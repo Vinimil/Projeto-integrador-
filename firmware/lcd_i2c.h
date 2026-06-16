843239f0d0a359ca65f4d27ec7002ef740857289
Vinimil <112878003+Vinimil@users.noreply.github.com>
2026-06-16 18:52:03 -0300

    Add lcd_i2c.h header file for LCD functions

---
+++ firmware/lcd_i2c.h
@@ -0,0 +1,20 @@
+#ifndef LCD_I2C_H
+#define LCD_I2C_H
+
+#include <stdint.h>
+
+void lcd_init_display(void);
+
+void lcd_clear(void);
+
+void lcd_set_cursor(uint8_t col, uint8_t row);
+
+void lcd_print(const char *text);
+
+void lcd_tela_pronto(float volume);
+
+void lcd_tela_dosando(float dose);
+
+void lcd_tela_final(float dose);
+
+#endif
